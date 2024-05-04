b64b3b22976362a31267905342a9b88608cd245f
Trpdjke <trpdjkework@gmail.com>
2024-05-04 20:58:55 +0300

    2

--- src/pages/router.py
+++ src/pages/router.py
@@ -8,6 +8,7 @@ router = APIRouter(
 
 templates = Jinja2Templates(directory="templates")
 
+
 @router.get("/chat")
 def get_chat_page(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
